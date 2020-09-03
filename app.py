c363dd708596ed8bafd6aa32c3aed28e26bbd915
Luke Murphy <lukewm@riseup.net>
2020-09-03 16:41:54 +0200

    Remove bad hack

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from os import environ
 
 from flask import Flask, jsonify
 
-# NOTE(decentral1se): some black magic to get the RPI happy
-environ["DISPLAY"] = ":0"
-environ["XAUTHORITY"] = "/home/decentral1se/.Xauthority"
-
 from pyautogui import click
 
 app = Flask(__name__)
